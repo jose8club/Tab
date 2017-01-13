4d219b61de0aaad33252080c3193c2a7c57f589a
jose8club <jose8club@gmail.com>
2017-01-13 17:53:26 -0300

    Negacion de la aplicacion a los no NFC

--- app/src/main/java/jose/tab/activity/MainActivity.java
+++ app/src/main/java/jose/tab/activity/MainActivity.java
@@ -1,11 +1,13 @@
 package jose.tab.activity;
 
 import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import jose.tab.R;
 
@@ -21,6 +23,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private Button btnIn, btnOut;
 
+    // Establece la duracoin en pantalla del splash screen
+    private static final long SPLASH_SCREEN_DELAY = 2000;
+
+    /**
+     * Establece errores NFC
+     */
+    public static final String ERROR_ENABLE = "No esta activada la funcionalidad NFC";
+    public static final String ERROR_NFC = "Este dispositivo no soporta NFC";
+
+    /**
+     * Adaptador NFC, usado para comprobar si posee NFC el equipo o no
+     */
+    NfcAdapter nfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +51,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          */
         btnIn.setOnClickListener(this);
         btnOut.setOnClickListener(this);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(nfcAdapter==null){
+            //detecta si el dispositivo tiene o no NFC
+            Toast.makeText(this, ERROR_NFC, Toast.LENGTH_LONG).show();
+            finish();
+        }
+        if(!nfcAdapter.isEnabled()){
+            //detecta si el dispositivo tiene activado su NFC
+            Toast.makeText(this,ERROR_ENABLE, Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 
     /**
--- app/src/main/java/jose/tab/activity/SplashScreen.java
+++ app/src/main/java/jose/tab/activity/SplashScreen.java
@@ -6,8 +6,10 @@ import java.util.TimerTask;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Toast;
 
 import jose.tab.R;
 
@@ -15,6 +17,17 @@ public class SplashScreen extends Activity {
 
     // Establece la duracoin en pantalla del splash screen
     private static final long SPLASH_SCREEN_DELAY = 2000;
+
+    /**
+     * Establece errores NFC
+     */
+    //public static final String ERROR_ENABLE = "No esta activada la funcionalidad NFC";
+    //public static final String ERROR_NFC = "Este dispositivo no soporta NFC";
+
+    /**
+     * Adaptador NFC, usado para comprobar si posee NFC el equipo o no
+     */
+    //NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +58,20 @@ public class SplashScreen extends Activity {
         // Simula un proceso de carga en el inicio de la aplicación.
         Timer timer = new Timer();
         timer.schedule(task, SPLASH_SCREEN_DELAY);
+
+        /*
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(nfcAdapter==null){
+            //detecta si el dispositivo tiene o no NFC
+            Toast.makeText(this, ERROR_NFC, Toast.LENGTH_LONG).show();
+            finish();
+        }
+        if(!nfcAdapter.isEnabled()){
+            //detecta si el dispositivo tiene activado su NFC
+            Toast.makeText(this,ERROR_ENABLE, Toast.LENGTH_LONG).show();
+            finish();
+        }
+        */
     }
 
 }
